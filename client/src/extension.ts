cd86f77d1e1bd16e385e5f2979326a2810e15f72
Michael Lin <michael.lin1@ibm.com>
2018-04-20 12:35:54 -0400

    fix broken tsconfig that failed to create production bundle

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -5,7 +5,7 @@ import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } f
 export function activate(context: ExtensionContext) {
 
   // The server is implemented in node
-  let serverModule = context.asAbsolutePath(path.join('server', 'server.js'));
+  let serverModule = context.asAbsolutePath(path.join('server', 'out', 'server.js'));
   // The debug options for the server
   let debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
 
