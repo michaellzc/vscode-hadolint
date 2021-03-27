michaellzc/vscode-hadolint

commit b03df4899eb2274ec219a30747f2ae7a83590075

feat: add multi-root workspace support

server/src/server.ts
@@ -1,127 +1,129 @@
-'use strict';
-
 import {
-  IPCMessageReader, IPCMessageWriter, createConnection,
-  IConnection, TextDocuments, TextDocument,
-  Diagnostic, DiagnosticSeverity, InitializeResult
-} from 'vscode-languageserver';
-import URI from 'vscode-uri';
-import * as hadolintService from './service/hadolint';
+	Diagnostic,
+	DiagnosticSeverity,
+	DidChangeConfigurationNotification,
+	ProposedFeatures,
+	TextDocumentSyncKind,
+	TextDocuments,
+	createConnection,
+} from "vscode-languageserver/node";
+import {URI} from "vscode-uri";
+import * as hadolintService from "./service/hadolint";
+import {getFileSystemPath} from "./utils";
+import {TextDocument} from "vscode-languageserver-textdocument";
 
-// The settings interface describe the server relevant settings part
-interface Settings {
-  hadolint: HadolintSettings;
-}
-
-// These are the example settings we defined in the client's package.json
-// file
 interface HadolintSettings {
-  maxNumberOfProblems: number;
-  outputLevel: string;
-  hadolintPath: string;
+	maxNumberOfProblems: number;
+	outputLevel: string;
+	hadolintPath: string;
 }
 
 const HadolintSeverity = {
-  error: DiagnosticSeverity.Error,
-  warning: DiagnosticSeverity.Warning,
-  info: DiagnosticSeverity.Information,
-  hint: DiagnosticSeverity.Hint
+	error: DiagnosticSeverity.Error,
+	warning: DiagnosticSeverity.Warning,
+	info: DiagnosticSeverity.Information,
+	hint: DiagnosticSeverity.Hint,
 };
 
-// Create a connection for the server. The connection uses Node's IPC as a transport
-let connection: IConnection = createConnection(new IPCMessageReader(process), new IPCMessageWriter(process));
-// Create a simple text document manager. The text document manager
-// supports full document sync only
-let documents: TextDocuments = new TextDocuments();
+// Creates the LSP connection
+const connection = createConnection(ProposedFeatures.all);
 
-// Make the text document manager listen on the connection
-// for open, change and close text document events
+// Create a manager for open text documents
+let documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
+
+// The workspace folder this server is operating on
+let workspaceFolder: string | null;
+
+async function getSettings(): Promise<HadolintSettings> {
+	const result = await connection.workspace.getConfiguration({
+		section: "hadolint",
+	});
+	return result;
+}
+
+async function validateTextDocument(textDocument: TextDocument): Promise<void> {
+	const settings = await getSettings();
+	connection.console.log(
+		`[hadolint(${process.pid}) ${workspaceFolder}] Current settings: ${JSON.stringify(
+			settings,
+		)}`,
+	);
+
+	let diagnostics: Array<Diagnostic> = [];
+	let lines = textDocument.getText().split(/\r?\n/g);
+	let dockerfilePath = getFileSystemPath(URI.parse(textDocument.uri));
+
+	try {
+		const hadolintResults = hadolintService.lint(
+			dockerfilePath,
+			settings.hadolintPath,
+			getFileSystemPath(URI.parse(workspaceFolder)),
+		);
+		// Format diagnostics
+		hadolintResults.forEach((result, index) => {
+			if (index > settings.maxNumberOfProblems) {
+				return;
+			}
+			let diagnosic: Diagnostic = {
+				// rome-ignore lint/ts/noExplicitAny
+				severity: (<any>HadolintSeverity)[settings.outputLevel],
+				range: {
+					start: {line: result.lineNumber - 1, character: 0},
+					end: {
+						line: result.lineNumber - 1,
+						character: lines[result.lineNumber - 1].length,
+					},
+				},
+				message: `[hadolint] ${result.message} (${result.rule})`,
+			};
+			diagnostics.push(diagnosic);
+		});
+		// Send the computed diagnostics to VSCode.
+		connection.sendDiagnostics({uri: textDocument.uri, diagnostics});
+	} catch (err) {
+		connection.window.showErrorMessage(`hadolint: ${err.message}`);
+	}
+}
+
+documents.onDidOpen((event) => {
+	connection.console.log(
+		`[hadolint(${process.pid}) ${workspaceFolder}] Document opened: ${event.document.uri}`,
+	);
+	validateTextDocument(event.document);
+});
+documents.onDidSave((event) => {
+	validateTextDocument(event.document);
+});
 documents.listen(connection);
 
-// Validate Dockerfile when file is opened
-documents.onDidOpen((change) => {
-  validateTextDocument(change.document);
+connection.onInitialize((params) => {
+	workspaceFolder = params.rootUri;
+	connection.console.log(
+		`[hadolint(${process.pid}) ${workspaceFolder}] Started and initialize received`,
+	);
+	return {
+		capabilities: {
+			textDocumentSync: {
+				openClose: true,
+				change: TextDocumentSyncKind.None,
+			},
+		},
+	};
 });
 
-// Revalidate Dockerfile when file is saved
-documents.onDidSave((change) => {
-  validateTextDocument(change.document);
+connection.onInitialized(() => {
+	connection.client.register(DidChangeConfigurationNotification.type, undefined);
 });
 
-// After the server has started the client sends an initialize request. The server receives
-// in the passed params the rootPath of the workspace plus the client capabilities.
-connection.onInitialize((_params): InitializeResult => {
-  return {
-    capabilities: {
-      // Tell the client that the server works in FULL text document sync mode
-      textDocumentSync: documents.syncKind
-    }
-  };
+connection.onDidChangeConfiguration(() => {
+	documents.all().forEach(validateTextDocument);
 });
 
-// hold the maxNumberOfProblems setting
-let maxNumberOfProblems: number;
-let outputLevel: string;
-let hadolintPath: string;
-
-function validateTextDocument(textDocument: TextDocument): void {
-  let diagnostics: Diagnostic[] = [];
-  let lines = textDocument.getText().split(/\r?\n/g);
-  let dockerfilePath = getFileSystemPath(URI.parse(textDocument.uri));
-
-  try {
-    const hadolintResults = hadolintService.lint(dockerfilePath, hadolintPath);
-    // Format diagnostics
-    hadolintResults.forEach((result, index) => {
-      if (index > maxNumberOfProblems) { return; }
-      let diagnosic: Diagnostic = {
-        severity: (<any>HadolintSeverity)[outputLevel],
-        range: {
-          start: { line: result.lineNumber-1, character: 0 },
-          end: { line: result.lineNumber-1, character: lines[result.lineNumber-1].length }
-        },
-        message: `[hadolint] ${result.message} (${result.rule})`
-      };
-      diagnostics.push(diagnosic);
-    });
-    // Send the computed diagnostics to VSCode.
-    connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
-  } catch (err) {
-    connection.window.showErrorMessage(`hadolint: ${err.message}`);
-  }
-}
-
-// Source: https://github.com/Microsoft/vscode-eslint/blob/master/server/src/eslintServer.ts#L269
-function getFileSystemPath(uri: URI): string {
-  let result = uri.fsPath;
-  if (process.platform === 'win32' && result.length >= 2 && result[1] === ':') {
-    // Node by default uses an upper case drive letter and ESLint uses
-    // === to compare pathes which results in the equal check failing
-    // if the drive letter is lower case in th URI. Ensure upper case.
-    return result[0].toUpperCase() + result.substr(1);
-  } else {
-    return result;
-  }
-}
-
-// The settings have changed. Is send on server activation
-// as well.
-connection.onDidChangeConfiguration((change) => {
-  let settings = <Settings>change.settings;
-  maxNumberOfProblems = settings.hadolint.maxNumberOfProblems || 100;
-  outputLevel = settings.hadolint.outputLevel || 'warning';
-  hadolintPath = settings.hadolint.hadolintPath;
-  // Revalidate any open text documents
-  documents.all().forEach(validateTextDocument);
+connection.onDidChangeWatchedFiles(() => {
+	connection.console.log(
+		`[hadolint(${process.pid}) ${workspaceFolder}] Detected changes in watched files`,
+	);
+	documents.all().forEach(validateTextDocument);
 });
 
-connection.onDidChangeWatchedFiles((_change) => {
-  // Detect .hadolint.yaml change
-  // revalidate all documents
-  documents.all().forEach(document => {
-    validateTextDocument(document);
-  });
-});
-
-// Listen on the connection
 connection.listen();

.config/rome.rjson
@@ -1,0 +1,9 @@
+// For configuration documentation see http://romefrontend.dev/#project-configuration
+name: "hadolint"
+lint: {
+	ignore: [
+		".history"
+		".vscode"
+]
+}
+root: true

.editorconfig
@@ -1,22 +1,7 @@
-root = true
-
-[*]
+[{*.js, *.json, *.rjson, *.ts}]
 end_of_line = lf
-insert_final_newline = true
-
-[*.{js,jsx,ts}]
-indent_style = space
-indent_size = 2
-charset = utf-8
 trim_trailing_whitespace = true
 insert_final_newline = true
-end_of_line = lf
-# editorconfig-tools is unable to ignore longs strings or urls
-max_line_length = null
-
-[*.{yml,json}]
-indent_style = space
-indent_size = 2
-
-[Makefile]
+charset = utf-8
 indent_style = tab
+indent_size = 2

.travis.yml
@@ -1,16 +1,0 @@
-language: node_js
-cache: yarn
-node_js:
-  - "10"
-before_install:
-  - if [ -n "$TRAVIS_TAG" ]; then export TAG=$TRAVIS_TAG; fi
-  - curl -o- -L https://yarnpkg.com/install.sh | bash -s -- --version 1.12.3
-  - export PATH="$HOME/.yarn/bin:$PATH"
-install:
-  - yarn install
-script:
-  - yarn vscode:prepublish
-  - yarn lint
-after_success:
-  - chmod +x scripts/publish.sh
-  - if [ -n "$TRAVIS_TAG" ]; then scripts/publish.sh; fi

.vscodeignore
@@ -1,9 +1,8 @@
 .vscode/**
-client/src/**
-server/src/**
+**/*.ts
 **/*.map
 .gitignore
 **/tsconfig.json
 **/tsconfig.base.json
+contributing.md
 .travis.yml
-*.log

LICENSE
@@ -1,6 +1,6 @@
 MIT License
 
-Copyright (c) 2018 Michael Lin
+Copyright (c) 2021 Michael Lin
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal

client/src/command/select-executable.ts
@@ -1,9 +1,10 @@
-import * as which from 'which';
-import * as vscode from 'vscode';
+import * as which from "which";
+// rome-ignore resolver/notFound
+import {window, workspace} from "vscode";
 
 export async function selectExecutable() {
-  const hadolintExectuables = which.sync('hadolint', { nothrow: true, all: true });
-  const selectedExectuable = await vscode.window.showQuickPick(hadolintExectuables);
-  const config = vscode.workspace.getConfiguration('hadolint');
-  return config.update('hadolintPath', selectedExectuable, true);
+	const hadolintExectuables = which.sync("hadolint", {nothrow: true, all: true});
+	const selectedExectuable = await window.showQuickPick(hadolintExectuables);
+	const config = workspace.getConfiguration("hadolint");
+	return config.update("hadolintPath", selectedExectuable, true);
 }

commit 9ab0e1ae87de2c9134b3e2b6f26d2e1a973ac8bc

fix: revalidate on save
regression from b03df48

fix #45

server/src/server.ts
@@ -33,7 +33,7 @@ console.log = connection.console.log.bind(connection.console);
 console.error = connection.console.error.bind(connection.console);
 
 // Create a manager for open text documents
-let documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
+let documents: TextDocuments<TextDocument>;
 
 // The workspace folder this server is operating on
 let workspaceFolder: string | null;
@@ -100,19 +100,27 @@ async function validateTextDocument(textDocument: TextDocument): Promise<void> {
 	}
 }
 
-documents.onDidOpen((event) => {
-	connection.console.log(
-		`[hadolint(${process.pid}) ${workspaceFolder}] Document opened: ${event.document.uri}`,
-	);
-	validateTextDocument(event.document);
-});
-documents.onDidSave((event) => {
-	validateTextDocument(event.document);
-});
-documents.listen(connection);
+function setupDocumentsListeners() {
+	documents.listen(connection);
+	documents.onDidOpen((event) => {
+		connection.console.log(
+			`[hadolint(${process.pid}) ${workspaceFolder}] Document is opened: ${event.document.uri}`,
+		);
+		validateTextDocument(event.document);
+	});
+
+	documents.onDidSave((event) => {
+		connection.console.log(
+			`[hadolint(${process.pid}) ${workspaceFolder}] Document is saved: ${event.document.uri}`,
+		);
+		validateTextDocument(event.document);
+	});
+}
 
 connection.onInitialize((params) => {
 	workspaceFolder = params.rootUri;
+	documents = new TextDocuments(TextDocument);
+	setupDocumentsListeners();
 	connection.console.log(
 		`[hadolint(${process.pid}) ${workspaceFolder}] Started and initialize received`,
 	);
@@ -120,6 +128,9 @@ connection.onInitialize((params) => {
 		capabilities: {
 			textDocumentSync: {
 				openClose: true,
+				save: {
+					includeText: false,
+				},
 				change: TextDocumentSyncKind.None,
 			},
 		},