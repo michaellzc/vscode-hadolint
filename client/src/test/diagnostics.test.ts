bba278c904dc12cea9552682a51709cebf32260e
Michael Lin <linzichunzf@hotmail.com>
2021-05-28 15:05:33 -0600

    chore(test): add e2e test

---
+++ client/src/test/diagnostics.test.ts
@@ -0,0 +1,53 @@
+// rome-ignore resolver/notFound: runtime dependency that doesn't exist
+import * as vscode from "vscode";
+import * as assert from "assert";
+import {activate, getDocUri} from "./helper";
+
+suite(
+	"Should get diagnostics",
+	() => {
+		const docUri = getDocUri("Dockerfile");
+		console.info(`Openning fixture ${docUri}`);
+
+		test(
+			"Diagnoses Always tag the version of an image explicitly (DL3006)",
+			async () => {
+				await testDiagnostics(
+					docUri,
+					[
+						{
+							message: "[hadolint] warning: Always tag the version of an image explicitly (DL3006)",
+							range: toRange(0, 0, 0, 11),
+							severity: vscode.DiagnosticSeverity.Warning,
+							source: "dockerfile",
+						},
+					],
+				);
+			},
+		);
+	},
+);
+
+function toRange(sLine: number, sChar: number, eLine: number, eChar: number) {
+	const start = new vscode.Position(sLine, sChar);
+	const end = new vscode.Position(eLine, eChar);
+	return new vscode.Range(start, end);
+}
+
+async function testDiagnostics(
+	docUri: vscode.Uri,
+	expectedDiagnostics: vscode.Diagnostic[],
+) {
+	await activate(docUri);
+
+	const actualDiagnostics = vscode.languages.getDiagnostics(docUri);
+
+	assert.equal(actualDiagnostics.length, expectedDiagnostics.length);
+
+	expectedDiagnostics.forEach((expectedDiagnostic, i) => {
+		const actualDiagnostic = actualDiagnostics[i];
+		assert.equal(actualDiagnostic.message, expectedDiagnostic.message);
+		assert.deepEqual(actualDiagnostic.range, expectedDiagnostic.range);
+		assert.equal(actualDiagnostic.severity, expectedDiagnostic.severity);
+	});
+}
